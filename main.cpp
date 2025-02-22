187e33b53ae43f31b008461404da9e5f087a93c3
LeslyAmaya <lamayal@miumg.edu.gt>
2025-02-21 23:49:35 -0600

    Ejer1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+using namespace std;
+//Función para determinar si un numero  ingresado es positivo o negativo o cero
+int main() {
+    int numero;
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+    if (numero > 0) {
+        cout << "El numero es positivo."<<endl;
+    } else if (numero < 0) {
+        cout << "El numero es negativo."<<endl;
+    } else {
+        cout << "El numero es cero."<<endl;
+    }
+    return 0;
+}
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
 //  Also, you can try interactive lessons for CLion by selecting
