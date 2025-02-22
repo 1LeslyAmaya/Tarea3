77afdfa5fce5afb97d61429f667c18661666c206
LeslyAmaya <lamayal@miumg.edu.gt>
2025-02-22 01:04:18 -0600

    Ejer6

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <cctype>
+#include <cmath>
 using namespace std;
+
+bool esPrimo(int nu) {
+    if(nu  < 2) return false;
+    for(int i = 2; i <= sqrt(nu); i++){
+        if(nu % i == 0)
+            return false;
+    }
+    return true;
+}
+
 //Función para determinar si un numero  ingresado es positivo o negativo o cero
 int main() {
     int numero;
@@ -22,42 +34,49 @@ int main() {
         cout << "El numero es impar." << endl;
     //Determinar de tres numeros cual es el mayor
     int main;{
-    double a;
-    double b;
-    double c;
-    cout << "Ingrese tres numeros (separados por espacio): " ;
-    cin >> a;
-    cin >> b;
-    cin >> c;
-    double mayor = a;
-    if(b > mayor)
-        mayor = b;
-    if(c > mayor)
-        mayor = c;
-    cout << "El mayor es: " << mayor << endl;
-}
-//determinar un año ingresado es bisiesto o no
+        double a;
+        double b;
+        double c;
+        cout << "Ingrese tres numeros (separados por espacio): " ;
+        cin >> a;
+        cin >> b;
+        cin >> c;
+        double mayor = a;
+        if(b > mayor)
+            mayor = b;
+        if(c > mayor)
+            mayor = c;
+        cout << "El mayor es: " << mayor << endl;
+    }
+    //determinar un año ingresado es bisiesto o no
     int anio;
     {
-    cout << "Ingrese un anio: ";
-    cin >> anio;
-    if((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0))
-        cout << anio << " es un anio bisiesto." << endl;
-    else
-        cout << anio << " no es un anio bisiesto." << endl;
+        cout << "Ingrese un anio: ";
+        cin >> anio;
+        if((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0))
+            cout << anio << " es un anio bisiesto." << endl;
+        else
+            cout << anio << " no es un anio bisiesto." << endl;
     }
- //Determinar si es letra ingresada por el usuario es vocal o consonante
+    //Determinar si es letra ingresada por el usuario es vocal o consonante
     char letra;
     cout << "Ingrese una letra: ";
     cin >> letra;
-        letra = tolower(letra);
+    letra = tolower(letra);
     if(letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u')
         cout << "La letra es una vocal." << endl;
     else if(isalpha(letra))
         cout << "La letra es una consonante." << endl;
     else
         cout << "El caracter ingresado no es una letra." << endl;
-
+    //Verificar si es un numero primo o no
+    int nu;
+    cout << "Ingrese un numero entero: ";
+    cin >> nu;
+    if(esPrimo(nu))
+        cout << nu << " es primo." << endl;
+    else
+        cout << nu << " no es primo." << endl;
 
     return 0;
 }
